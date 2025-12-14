843601d92befaa24abd2842f0d5997bab727ecf7
Colomba Vellotti <vellotti.colomba1996@gmail.com>
2025-12-14 16:36:01 +0000

    Configure Inonic Storage in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,25 @@
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
+import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 
-import { routes } from './app/app.routes';
+import { IonicStorageModule } from '@ionic/storage-angular';
+
+import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
+
+if (environment.production) {
+  enableProdMode();
+}
 
 bootstrapApplication(AppComponent, {
   providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideRouter(routes),
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+
+    // Ionic Storage (Angular Standalone)
+    importProvidersFrom(IonicStorageModule.forRoot()),
   ],
 });
